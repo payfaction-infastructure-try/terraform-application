0ec4c8425f17941f6200446fc69209c5ad0896ee
Ilya Kohanovskiy <i.kohanvs@gmail.com>
2022-03-23 18:05:56 +0400

    changes

--- locals.tf
+++ locals.tf
@@ -5,10 +5,11 @@ locals {
   aws_alb_target_group_name           = "${var.AWS_RESOURCE_NAME_PREFIX}-alb-target-group"
   aws_ecs_service_security_group_name = "${var.AWS_RESOURCE_NAME_PREFIX}-ecs-service-security-group"
 
-  vpc_id               = data.terraform_remote_state.main_infrastructure.outputs.vpc_id
-  cluster_id           = data.terraform_remote_state.main_infrastructure.outputs.cluster_id
-  lb_listener          = data.terraform_remote_state.main_infrastructure.outputs.lb_listener
-  private_subnets      = data.terraform_remote_state.main_infrastructure.outputs.private_subnets
-  load_balancer_ip     = data.terraform_remote_state.main_infrastructure.outputs.load_balancer_ip
-  lb_security_group_id = data.terraform_remote_state.main_infrastructure.outputs.load_balancer_security_group_id
+  vpc_id                  = data.terraform_remote_state.main_infrastructure.outputs.vpc_id
+  cluster_id              = data.terraform_remote_state.main_infrastructure.outputs.cluster_id
+  lb_listener             = data.terraform_remote_state.main_infrastructure.outputs.lb_listener
+  private_subnets         = data.terraform_remote_state.main_infrastructure.outputs.private_subnets
+  load_balancer_ip        = data.terraform_remote_state.main_infrastructure.outputs.load_balancer_ip
+  lb_default_target_group = data.terraform_remote_state.main_infrastructure.outputs.alb_default_target_group
+  lb_security_group_id    = data.terraform_remote_state.main_infrastructure.outputs.load_balancer_security_group_id
 }
