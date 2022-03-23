e41708252bce05adf038306149560faffb122844
Ilya Kohanovskiy <i.kohanvs@gmail.com>
2022-03-23 19:03:29 +0400

    3

--- lb-listener-rule.tf
+++ lb-listener-rule.tf
@@ -12,13 +12,13 @@ resource "aws_lb_listener_rule" "app_listener_rule" {
     dynamic "host_header" {
       for_each = { for i in var.LISTENER_RULE_CONDITIONS : i.field => i if i.field == "host-header" }
       content {
-        values = [host_header.value.value]
+        values = host_header.value.values
       }
     }
     dynamic "path_pattern" {
       for_each = { for i in var.LISTENER_RULE_CONDITIONS : i.field => i if i.field == "path-pattern" }
       content {
-        values = [path_pattern.value.value]
+        values = path_pattern.value.values
       }
     }
   }
